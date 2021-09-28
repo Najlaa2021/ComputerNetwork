d6dcaa2c2dbabe439a8371eb8a46d3d568f835d5
Najlaa2021 <90162733+Najlaa2021@users.noreply.github.com>
2021-09-28 10:23:19 -0400

    .

--- solution.py
+++ solution.py
@@ -11,23 +11,23 @@ def webServer(port=13331):
     # Fill in end
     while True:
         # Establish the connection
-        print('openning accept...')
-        print('Serving @ 127.0.0.1:{}'.format(port))
+        #print('openning accept...')
+        #print('Serving @ 127.0.0.1:{}'.format(port))
         connectionSocket, addr = serverSocket.accept() #ACCEPTING THE INCOMING CLIENT REQUEST
-        print('Ready to serve...')
+        #print('Ready to serve...')
         try:
             try:
                 message = connectionSocket.recv(1024).decode() # Fill in start    #Fill in end
                 filename = message.split()[1]
                 f = open(filename[1:])
-                print("Serving Client: {}".format(filename))
+                #print("Serving Client: {}".format(filename))
                 outputdata = f.readlines()
                 f.close()
                 # Fill in start     #Fill in end
                 # Send one HTTP header line into socket.
                 # Fill in start
                 response = "HTTP/1.1 200 OK\r\n\r\n"
-                print(response)
+                #print(response)
                 connectionSocket.send(response.encode("utf-8"))
                 for i in range(0, len(outputdata)):
                     connectionSocket.send(outputdata[i].encode("utf-8"))
@@ -38,7 +38,7 @@ def webServer(port=13331):
                 # Fill in start
                 errorNotFound = "HTTP/1.1 404 Not Found\r\n\r\n"
                 connectionSocket.send(errorNotFound.encode("utf-8"))
-                print(errorNotFound)
+                #print(errorNotFound)
         # Fill in end
         # Close client socket
         # Fill in start
@@ -53,4 +53,3 @@ def webServer(port=13331):
 if __name__ == "__main__":
     webServer(13331)
     
-print("naj")
